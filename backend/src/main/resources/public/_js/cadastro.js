e44dea4bdd34eb1d475b10485ca93f94dc1eff9a
Vilson Júnior <vilsonjrcorrea@gmail.com>
2018-11-27 15:22:39 -0200

    Novo conceito

---
+++ backend/src/main/resources/public/_js/cadastro.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    //chamada do menu fixo
+    $(".menuFixo").load("menuFixo.html");
+
+    //chamada do rodapé fixo
+    $(".footerFixo").load("footerFixo.html");
+
+});
---
+++ backend/src/main/resources/public/_js/menuFixo.js
@@ -0,0 +1,49 @@
+$(function () {
+
+    let menuOpen = false;
+
+    /**
+     * Função animada de abertura/fechamento do menu
+     */
+    $(".menu-link").on("click", function () {
+        if (menuOpen === false) {
+            $("nav").animate({"height": "550px"}, 250);
+            $(".menu-float").animate({"margin-left": "0"}, 400);
+            $(".seta").html('<img src="../img/seta cima giz.png" height="40"/>');
+            menuOpen = true;
+        } else {
+            $("nav").animate({"height": "55px"}, 250);
+            $(".menu-float").animate({"margin-left": "-2000px"}, 100);
+            $(".seta").html('<img src="../img/seta baixo giz.png" height="40"/>');
+            menuOpen = false;
+        }
+    });
+
+
+    /**
+     * Função que recolhe o menu com a tecla ESC
+     */
+    $(document).keyup(function (e) {
+        if(e.keyCode === 27) {
+            if(menuOpen === true) {
+                $("nav").animate({"height": "55px"}, 250);
+                $(".menu-float").animate({"margin-left": "-2000px"}, 100);
+                $(".seta").html('<img src="../img/seta baixo giz.png" height="40"/>');
+                menuOpen = false;
+            }
+        }
+    });
+
+    /**
+     * Função que recolhe o menu ao clicar fora dele
+     */
+    $("section").on('click', function () {
+        if(menuOpen === true) {
+            $("nav").animate({"height": "55px"}, 250);
+            $(".menu-float").animate({"margin-left": "-2000px"}, 100);
+            $(".seta").html('<img src="../img/seta baixo giz.png" height="40"/>');
+            menuOpen = false;
+        }
+    });
+
+});
