271b6ddd3e35ac9575178dcabe2ef35fe4e387d6
Cristiano A. Flores <cristianoarrudaflores@hotmail.com>
2018-11-28 17:32:54 -0200

    tela institucional 99% desenvolvida

--- frontend/app/_js/index.js
+++ frontend/app/_js/index.js
@@ -10,13 +10,8 @@ $(function () {
             statusCode: {
                 200: function (response) {
                     response.responseText;
-<<<<<<< HEAD
                     // window.location.href = "institucional.html";
                     if(window.location.href.includes('_html/')){
-=======
-                    // window.location.href = "cadastro.html";
-                    if(window.location.href.includes('_html/' )){
->>>>>>> 1bc5f323d8c62668cc0e59b94a746af822fe5f45
                         window.location.replace("dashboard.html");
                     }else{
                         window.location.replace("_html/dashboard.html");
